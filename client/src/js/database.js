94bcc17587082e4b0ed7bc4db9cdefc5abb44be5
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-04-01 13:23:37 -0400

    modifying database to improve performance

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,12 +18,10 @@ export const putDb = async (content) => {
   const contactDb = await openDB('jate', 1);
   const tx = contactDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id:1, value: content });
-  const result = await req
+  const request = store.put({ value: content });
+  const result = await request;
 
   console.log('Content added to the database', result);
-
-  console.error('putDb not implemented');
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
